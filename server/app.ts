6b2b4ad4dabad0f2dba06ce41b2d7d284b7bc35d
shadowgag1 <55539405+shadowgag1@users.noreply.github.com>
2025-11-28 13:04:55 +0530

    Add server/app.ts

---
+++ server/app.ts
@@ -0,0 +1,67 @@
+import { type Server } from "node:http";
+
+import express, {
+  type Express,
+  type Request,
+  Response,
+  NextFunction,
+} from "express";
+
+import { registerRoutes } from "./routes";
+
+export function log(message: string, source = "express") {
+  if (process.env.NODE_ENV === 'development') {
+    const formattedTime = new Date().toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    });
+    console.log(`${formattedTime} [${source}] ${message}`);
+  }
+}
+
+export const app = express();
+
+declare module 'http' {
+  interface IncomingMessage {
+    rawBody: unknown
+  }
+}
+app.use(express.json({
+  verify: (req, _res, buf) => {
+    req.rawBody = buf;
+  }
+}));
+app.use(express.urlencoded({ extended: false }));
+
+app.use((_req, _res, next) => {
+  next();
+});
+
+export default async function runApp(
+  setup: (app: Express, server: Server) => Promise<void>,
+) {
+  const server = await registerRoutes(app);
+
+  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+    const status = err.status || err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+
+    res.status(status).json({ message });
+    throw err;
+  });
+
+  await setup(app, server);
+
+  const port = parseInt(process.env.PORT || '5000', 10);
+  server.listen({
+    port,
+    host: "0.0.0.0",
+    reusePort: true,
+  }, () => {
+    if (process.env.NODE_ENV === 'development') {
+      log(`serving on port ${port}`);
+    }
+  });
+}
